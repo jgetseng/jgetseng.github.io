1a82e0d98f9c304396c7c6dfaa91845ab8259fb8
jgetseng <getseng19@gmail.com>
2022-05-31 09:32:57 +0800

    My Website 2.0.1

--- js/header.js
+++ js/header.js
@@ -7,12 +7,12 @@ const nav_ul = document.createElement("ul");
 const nav_li_essay = document.createElement("li");
 const nav_li_photopraph = document.createElement("li");
 const nav_li_thought = document.createElement("li");
-const nav_li_comment = document.createElement("li");
+/*const nav_li_comment = document.createElement("li");*/
 const nav_li_letter = document.createElement("li");
 const nav_essay = document.createElement("a");
 const nav_thought = document.createElement("a");
 const nav_photograph = document.createElement("a");
-const nav_comment = document.createElement("a");
+/*const nav_comment = document.createElement("a");*/
 const nav_letter = document.createElement("a");
 
 header_container.id = "header_container";
@@ -24,14 +24,14 @@ nav_li_letter.id = "nav_last";
 nav_essay.href = "/essay.html";
 nav_photograph.href = "/photograph.html";
 nav_thought.href = "/thought.html";
-nav_comment.href = "/comment.html";
+/*nav_comment.href = "/comment.html";*/
 nav_letter.href = "https://forms.gle/iUupMWH6DuynX3mi7";
 nav_letter.target = "_blank";
 
 nav_essay.innerText = "隨筆";
 nav_photograph.innerText = "隨拍";
 nav_thought.innerText = "隨想";
-nav_comment.innerText = "隨評";
+/*nav_comment.innerText = "隨評";*/
 nav_letter.innerText = "隨信";
 
 homepage.append(homepage_a);
@@ -40,12 +40,12 @@ header.append(header_container);
 nav_li_essay.append(nav_essay);
 nav_li_photopraph.append(nav_photograph);
 nav_li_thought.append(nav_thought);
-nav_li_comment.append(nav_comment);
+/*nav_li_comment.append(nav_comment);*/
 nav_li_letter.append(nav_letter);
 nav_ul.append(nav_li_essay);
 nav_ul.append(nav_li_photopraph);
 nav_ul.append(nav_li_thought);
-nav_ul.append(nav_li_comment);
+/*nav_ul.append(nav_li_comment);*/
 nav_ul.append(nav_li_letter);
 nav.append(nav_ul);
 header_container.append(nav);
