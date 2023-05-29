01ab3bc987933af15f23b0d34d7922552497f124
jgetseng <getseng19@gmail.com>
2023-05-29 20:25:55 +0800

    My Website 2.2.2

--- js/photograph.js
+++ js/photograph.js
@@ -1,5 +1,4 @@
 function showPhoto(imgs) {
     var photo_expanded = document.getElementById("photo_expanded");
-    /* document.getElementById("photo_expanded").style.tran = "1s"; */
     photo_expanded.src = imgs.src;
 }
