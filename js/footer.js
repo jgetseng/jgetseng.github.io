7fe0db9d35acdf6c6dff16fc61ce1840a94a3626
jgetseng <getseng19@gmail.com>
2025-08-31 09:44:35 +0800

    My Website 2.2.8

--- js/footer.js
+++ js/footer.js
@@ -1,47 +1,45 @@
-/*
 const footer = document.createElement("footer");
 const footer_ul = document.createElement("ul");
-const facebook = document.createElement("li");
-const facebook_a = document.createElement("a");
-const facebook_i = document.createElement("i");
 const instagram = document.createElement("li");
 const instagram_a = document.createElement("a");
 const instagram_i = document.createElement("i");
+const medium = document.createElement("li");
+const medium_a = document.createElement("a");
+const medium_i = document.createElement("i");
 const youtube = document.createElement("li");
 const youtube_a = document.createElement("a");
 const youtube_i = document.createElement("i");
 
 footer.id = "footer";
-facebook_i.className = "fab fa-facebook-square";
 instagram_i.className = "fab fa-instagram";
+medium_i.className = "fab fa-medium";
 youtube_i.className = "fab fa-youtube";
 
-facebook_a.href = "https://www.facebook.com/jgetseng";
 instagram_a.href = "https://www.instagram.com/jgetseng/";
+medium_a.href = "https://jgetseng.medium.com/";
 youtube_a.href = "https://www.youtube.com/channel/UCV8minFHgKlfRzkbSTJFokA";
 
-facebook_a.target = "_blank";
 instagram_a.target = "_blank";
+medium_a.target = "_blank";
 youtube_a.target = "_blank";
 
-facebook_a.append(facebook_i);
 instagram_a.append(instagram_i);
+medium_a.append(medium_i);
 youtube_a.append(youtube_i);
-facebook.append(facebook_a);
 instagram.append(instagram_a);
+medium.append(medium_a);
 youtube.append(youtube_a);
-footer_ul.append(facebook);
 footer_ul.append(instagram);
+footer_ul.append(medium);
 footer_ul.append(youtube);
 footer.append(footer_ul);
 document.body.append(footer);
-*/
 
 /*
         <footer id="footer">
             <ul>
-                <li><a href="https://www.facebook.com/jgetseng" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
                 <li><a href="https://www.instagram.com/jgetseng/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="https://jgetseng.medium.com/" target="_blank"><i class="fa fa-medium"></i></a></li>
                 <li><a href="https://www.youtube.com/channel/UCV8minFHgKlfRzkbSTJFokA" target="_blank"><i class="fab fa-youtube"></i></a></li>
             </ul>  
         </footer>
